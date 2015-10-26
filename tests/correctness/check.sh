09d3b1182771516edbed4b3207ae9aec43a710b8
Aetf <aetf@unlimitedcodeworks.xyz>
2015-10-25 20:43:39 -0400

    Better control of test script

--- tests/correctness/check.sh
+++ tests/correctness/check.sh
@@ -5,7 +5,8 @@ mkdir -p $2
 ./$1 > $2/orig
 ./$1.slicm > $2/res
 if diff -q $2/{orig,res}; then
-    echo "PASSED"
+    printf "[PASSED]"
 else
-    echo "FAILED"
+    printf "[FAILED]"
 fi
+echo " $1"
